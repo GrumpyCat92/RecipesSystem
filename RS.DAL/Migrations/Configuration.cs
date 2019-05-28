d811ecda1e73f067b769631a3cb7983fca7c53a1
GrumpyCat92 <nataliachymakova@gmail.com>
2019-05-28 08:50:50 +0300

    Update Configuration.cs

--- RS.DAL/Migrations/Configuration.cs
+++ RS.DAL/Migrations/Configuration.cs
@@ -15,28 +15,39 @@ namespace RS.DAL.Migrations
         protected override void Seed(RS.DAL.RSContext context)
         {
             context.Categories.AddOrUpdate(
+                e => e.Name,
                 new Category { Name = "Desert"});
             context.Categories.AddOrUpdate(
+                e => e.Name,
                 new Category { Name = "Salad" });
             context.Categories.AddOrUpdate(
+                e => e.Name,
                 new Category { Name = "Soup" });
             context.Categories.AddOrUpdate(
+                e => e.Name,
                 new Category { Name = "Drink" });
 
             context.TimesPrepaering.AddOrUpdate(
+                e => e.Name,
                 new TimePrepaering{ Period = "Less then 30 min"});
             context.TimesPrepaering.AddOrUpdate(
+                e => e.Name,
                 new TimePrepaering { Period = "From 30 min to 60 min" });
             context.TimesPrepaering.AddOrUpdate(
+                e => e.Name,
                 new TimePrepaering { Period = "More then 60 min" });
 
             context.Types.AddOrUpdate(
+                e => e.Name,
                 new Core.Type {Name = "Breakfast"});
             context.Types.AddOrUpdate(
+                e => e.Name,
                 new Core.Type { Name = "Dinner" });
             context.Types.AddOrUpdate(
+                e => e.Name,
                 new Core.Type { Name = "Lunch" });
             context.Types.AddOrUpdate(
+                e => e.Name,
                 new Core.Type { Name = "Celebration dish" });
         }
     }
