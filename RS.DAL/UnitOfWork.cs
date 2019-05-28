d0cf377b2a9c8fdef512aba436730fa90bf917b5
GrumpyCat92 <nataliachymakova@gmail.com>
2019-05-28 08:24:55 +0300

    Update UnitOfWork.cs

--- RS.DAL/UnitOfWork.cs
+++ RS.DAL/UnitOfWork.cs
@@ -19,14 +19,75 @@ namespace RS.DAL
         private IGenericRepository<TimePrepaering> timePrepaeringRep;
         private IGenericRepository<Core.Type> typeRep;
 
+        public UnitOfWork()
+        {
+            db = new RSContext();
+            categoryRep = new GenericRepository<Category>(db);
+            ingridientRep = new GenericRepository<Ingridient>(db);
+            ingridientForDishRep = new GenericRepository<IngridientForDish>(db);
+            recipeRep = new GenericRepository<Recipe>(db);
+            timePrepaeringRep = new GenericRepository<TimePrepaering>(db);
+            typeRep = new GenericRepository<Core.Type>(db);
+        }
+        
+        public IGenericRepository<Category> CategoryRep
+        {
+            get { return categoryRep; }
+        }
+        
+        public IGenericRepository<Ingridient> IngridientRep
+        {
+            get { return ingridientRep; }
+        }
+        
+        public IGenericRepository<IngridientForDish> IngridientForDishRep
+        {
+            get { return ingridientForDishRep; }
+        }
+        
+        public IGenericRepository<Recipe> RecipeRep
+        {
+            get { return recipeRep; }
+        }
+        
+        public IGenericRepository<TimePrepaering> TimePrepaeringRep
+        {
+            get { return timePrepaeringRep; }
+        }
+        
+        public IGenericRepository<Type> TypeRep
+        {
+            get { return typeRep; }
+        }
+        
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    categoryRep.Dispose();
+                    ingridientRep.Dispose();
+                    ingridientForDishRep.Dispose();
+                    recipeRep.Dispose();
+                    timePrepaeringRep.Dispose();
+                    typeRep.Dispose();
+                }
+                this.disposed = true;
+            }
         }
 
         public Task SaveAsync()
         {
-            throw new NotImplementedException();
+            await db.SaveChangesAsync();
         }
     }
 }
